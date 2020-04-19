67d07f24a67d985a5dad5237faac85166cc99ee6
Gandhar Bichkar <30771519+theSC0RP@users.noreply.github.com>
2020-04-20 03:26:34 +0530

    Update train.py

--- train.py
+++ train.py
@@ -176,3 +176,5 @@ if __name__ == "__main__":
 
         if (epoch + 1) % 10 == 0:
             torch.save(model.state_dict(), f"checkpoints/yolov3_ckpt_%d.pth" % epoch)
+            path = f"/content/gdrive/My Drive/yolov3_ckpt_%d.pth" % epoch 
+            torch.save(model.state_dict(), path)
