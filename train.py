d27b045b2d3490f62763208bf63fca45f6c35eb6
Gandhar Bichkar <30771519+theSC0RP@users.noreply.github.com>
2020-04-19 10:04:49 +0530

    Update train.py

--- train.py
+++ train.py
@@ -174,5 +174,5 @@ if __name__ == "__main__":
 #             print(AsciiTable(ap_table).table)
 #             print(f"---- mAP {AP.mean()}")
 
-        if epoch % opt.checkpoint_interval == 0:
+        if epoch == 99:
             torch.save(model.state_dict(), f"checkpoints/yolov3_ckpt_%d.pth" % epoch)
